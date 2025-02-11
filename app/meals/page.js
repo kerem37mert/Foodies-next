5ff13f45653865188c34695a0758ea2334bb81ae
kerem37mert <kerem37mert@yahoo.com>
2025-02-11 14:46:52 +0300

    db conneciton completed

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function() {
+export default async function() {
+
+  const meals = await getMeals();
     return(
       <>
         <header className={ classes.header }>
@@ -20,7 +23,7 @@ export default function() {
           </p>
         </header>
         <main className={ classes.main }>
-          <MealsGrid meals={[]} />
+          <MealsGrid meals={meals} />
         </main>
       </>
     );
